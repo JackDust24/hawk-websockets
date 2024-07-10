9e668430649ecfdbc1c57566c04ce2444ad15e9f
Jason Whittaker <jason.whittaker@7peakssoftware.com>
2024-07-10 13:33:05 +0700

    chore: applied tailwind to login and main page

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,13 +1,45 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import { MainPage } from './pages/main';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import './App.css';
+import './index.css';
 
 function App() {
   return (
     <>
-      <Navbar color='light' light>
+      {/* <Navbar color='light' light>
         <NavbarBrand href='/'>Real-time document editor</NavbarBrand>
-      </Navbar>
+      </Navbar> */}
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static' color='secondary'>
+          <Toolbar
+            variant='dense'
+            sx={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <IconButton
+              edge='start'
+              color='inherit'
+              aria-label='chat'
+              sx={{ mr: 2 }}
+            >
+              <WhatsAppIcon />
+            </IconButton>
+            <Typography
+              variant='h6'
+              color='inherit'
+              component='div'
+              sx={{ flexGrow: 1, textAlign: 'center', marginRight: 'auto' }}
+            >
+              HAWK Chat App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
       <MainPage />
     </>
   );
--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -1,46 +1,56 @@
 import React, { useState } from 'react';
 import useWebSocket from 'react-use-websocket';
 import { WS_URL } from '../api/websockets-helpers';
+import Box from '@mui/material/Box';
 
 export function LoginSection({ onLogin }) {
-    const [username, setUsername] = useState('');
-  
-    // Notify the server a new user has joined
-    useWebSocket(WS_URL, {
-      share: true,
-      filter: () => false,
-    });
-  
-    function logInUser() {
-      if (!username.trim()) {
-        return;
-      }
-      onLogin && onLogin(username);
+  const [username, setUsername] = useState('');
+
+  // Notify the server a new user has joined
+  useWebSocket(WS_URL, {
+    share: true,
+    filter: () => false,
+  });
+
+  function logInUser() {
+    if (!username.trim()) {
+      return;
     }
-  
-    return (
-      <div className='account'>
-        <div className='account__wrapper'>
-          <div className='account__card'>
-            <div className='account__profile'>
-              <p className='account__name'>Hello, user!</p>
-              <p className='account__sub'>Join to edit the document</p>
-            </div>
-            <input
-              name='username'
-              onInput={(e) => setUsername(e.target.value)}
-              className='form-control'
-            />
-            <button
-              type='button'
-              onClick={() => logInUser()}
-              className='btn btn-primary account__btn'
-            >
-              Join
-            </button>
+    onLogin && onLogin(username);
+  }
+  return (
+    <Box
+      sx={{
+        bgcolor: '#ecfeff',
+        height: '90vh',
+        display: 'flex',
+        overflowY: 'auto',
+        margin: '20px',
+      }}
+    >
+      <div className='m-auto p-4'>
+        <div className='bg-cyan-100 mt-7.5 mx-auto py-4 px-4 w-[500px] border-indigo-950 shadow-2xl border-2'>
+          <div className='text-left'>
+            <p className='my-2 uppercase text-4xl font-bold	leading-7'>
+              Welcome!
+            </p>
+            <p className='pt-2 my-2 text-xl text-slate-500'>Join the chat</p>
           </div>
+          <input
+            name='username'
+            onInput={(e) => setUsername(e.target.value)}
+            className='my-2 p-2 border-[1px] rounded border-gray-500 w-[300px]'
+            placeholder='Enter your username'
+          />
+          <button
+            type='button'
+            onClick={() => logInUser()}
+            className='flex bg-blue-500 hover:bg-blue-600 active:bg-blue-700 mx-auto mt-4 px-6 py-2 text-2xl rounded-md shadow-lg text-slate-100 font-extrabold'
+          >
+            Join
+          </button>
         </div>
       </div>
-    );
-  }
-  
+    </Box>
+  );
+}
